a7f5bacbf839e8b84b8a81e6d38f580ea453c335
anamaksimova <19cherrytree91@gmail.com>
2022-02-21 02:25:50 +0500

    end

--- js/script.js
+++ js/script.js
@@ -1,20 +1,62 @@
-const sliderThumbs = new Swiper('.slider-thumbs', {
-    loop: true,
-    spaceBetween: 20,
-    slidesPerView: 3,
-    centeredSlides:true,
-    loopedSlides:4,
+//burger menu
+const burger = document.querySelector('.burger')
+const navigation = document.querySelector('.navigation');
+const navigationClose = document.querySelector('.navigation__close');
+burger.addEventListener('click',
+    () => {
+        navigation.classList.add('navigation_active');
+    });
+
+navigationClose.addEventListener('click', () => {
+    navigation.classList.remove('navigation_active');
 });
+//music
+try {
+    const mute = document.querySelector('.mute__checkbox');
+    const audio = new Audio('audio/waterTower.mp3');
+    const checkMute = () => {
+        if (mute.checked) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    };
 
-sliderThumbs.on('click', (swiper) =>{
-    swiper.slideTo(swiper.clickedIndex)
-})
-const sliderMain = new Swiper('.slider-main', {
-    loop: true,
-    loopedSlides:4,
-    spaceBetween: 20,
+    if (mute) { setTimeout(checkMute, 2000) }
+    mute.addEventListener('change', checkMute);
+} catch { }
 
-});
+try {
+    const sliderThumbs = new Swiper('.slider-thumbs', {
+        loop: true,
+        spaceBetween: 20,
+        slidesPerView: 3,
+        centeredSlides: true,
+        loopedSlides: 4,
+    });
 
-sliderThumbs.controller.control = sliderMain;
-sliderMain.controller.control = sliderThumbs;
+    sliderThumbs.on('click', (swiper) => {
+        swiper.slideTo(swiper.clickedIndex)
+    })
+    const sliderMain = new Swiper('.slider-main', {
+        loop: true,
+        loopedSlides: 4,
+        spaceBetween: 20,
+
+    });
+
+    sliderThumbs.controller.control = sliderMain;
+    sliderMain.controller.control = sliderThumbs;
+
+    const videos = document.querySelectorAll('video');
+    sliderMain.on('slideChange', () => {
+        for (let i = 0; i < videos.length; i += 1) {
+            videos[i].pause();
+        }
+    });
+    const pagination = document.querySelector('.pagination');
+    const paginationButton = document.querySelector('.pagination__arrow');
+    paginationButton.addEventListener('click', ()=>{
+        pagination.classList.toggle('pagination_active')
+    })
+} catch { }
